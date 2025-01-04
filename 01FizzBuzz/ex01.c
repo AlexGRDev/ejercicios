f293c84daa462261b47734ab27cb9f1545bb4f1c
AlexGRDev <alexgaro2015@gmail.com>
2025-01-04 20:59:48 +0100

    add  01FizzBuzz/ - exercicie 01 finished

---
+++ 01FizzBuzz/ex01.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ex01.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alegarc4 <alegarc4@student.42barcelona>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/04 20:57:37 by alegarc4          #+#    #+#             */
+/*   Updated: 2025/01/04 20:57:37 by alegarc4         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+char    *fizzbuzz(int   *n)
+{
+        if (*n % 3 == 0 && *n % 5 == 0)
+        {
+                printf("fizzbuzz");
+        }
+        else if (*n % 3 == 0)
+        {
+                printf("fizz\n");
+        }
+        else if (*n % 5 == 0)
+        {
+                printf("buzz\n");
+        }
+        else
+        {
+                printf("%d\n", *n);
+        }
+        return(0);
+}
+
+int     main(void)
+{
+        int     n;
+        int     *ptr;
+
+        ptr = &n;
+        ptr = malloc(sizeof(int));
+        if (ptr == NULL) 
+        {
+                fprintf(stderr, "Memory allocation failed\n");
+                return (1);
+        }
+        *ptr = 1;
+        while (*ptr <= 100)
+        {
+                fizzbuzz(ptr);
+                *ptr += 1;
+        }
+        free(ptr);
+        return (0);
+}
