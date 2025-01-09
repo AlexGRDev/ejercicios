f35649cbb2dd1d094c86ce099ac9a4d86931263b
Alex Garcia Rodriguez <110727609+AlexGRDev@users.noreply.github.com>
2025-01-09 14:53:51 +0100

    ex03 finished

---
+++ 03Fibonacci/Fibonacci.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fibonacci.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alexgarc4 <alexgarc4@student.42barcelon    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/08 12:57:16 by alexgarc4         #+#    #+#             */
+/*   Updated: 2025/01/09 14:49:49 by alexgarc4        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int     fibibacci(int n, int memo[])
+{
+        if (memo[n] == 0)
+        {
+                if (n == 0)
+                        return 0;
+                else if (n == 1)
+                        return 1;
+                else
+                {
+                        memo[n] = fibibacci(n - 1, memo) + fibibacci(n - 2, memo);
+                        return memo[n];
+                }
+        }
+        else
+                return memo[n];
+}
+
+int main(int argc, char *argv[])
+{
+        if (argc != 2) 
+        {
+        printf("Usage: %s <size of the Fibonacci sequence>\n", argv[0]);
+        return 1;
+        }
+
+        int size = atoi(argv[1]);
+        if (size < 0) {
+                printf("Size must be a non-negative integer.\n");
+                return 1;
+        }
+
+        int *memo = (int *)malloc(sizeof(int) * (size + 1));
+        if (memo == NULL) {
+                printf("Memory allocation failed\n");
+                return 1;
+        }
+        
+        int i;
+        int *ptrI = &i;
+        ptrI = (int *)malloc(sizeof(int) * (size + 1));
+        if (ptrI == NULL) {
+                printf("Memory allocation failed\n");
+                return 1;
+        }
+        *ptrI = 0;
+        while ( *ptrI < size) {
+                printf("%d \n", fibibacci(*ptrI, memo));
+                *ptrI = *ptrI + 1;
+        }
+        free(memo);
+        free(ptrI);
+        return 0;        
+}
+
---
+++ 03Fibonacci/statement.md
@@ -0,0 +1,40 @@
+# Fibonacci Sequence
+
+## Descripción
+<p>
+        Este programa en C imprime los primeros 50 números de la sucesión de Fibonacci, comenzando desde 0.<br>
+        <p> 
+                La sucesión de Fibonacci es una secuencia matemática en la que cada número es la suma de los dos anteriores, comenzando por 0 y 1.
+        </p>
+        Los primeros números de la sucesión son:
+        0, 1, 1, 2, 3, 5, 8, 13...
+</p>
+
+
+## Requisitos
+
+- Compilador C (por ejemplo, GCC o CC)
+- Sistema operativo compatible (Linux, macOS, Windows)
+
+## Instrucciones de Uso
+
+1. Clona este repositorio en tu máquina local.
+
+```bash
+git clone https://github.com/AlexGRDev/ejercicios.git
+cd ~/ejercicios/03fibonacci
+```
+
+## Compilación y Ejecución
+1. Compila el código usando estas flags:
+```bash
+gcc -g -O2 -Wall -Werror -Wextra Fionacci.c -o programa
+```
+2. Ejecuta el programa:
+```bash
+./programa
+```
+## Ejemplo de salida
+```bash
+0 1 1 2 3 5 8 13
+```
