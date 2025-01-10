4b145994b3a68205d0d047609014d674741c0ed9
Alex Garcia Rodriguez <alexgaro2015@gmail.com>
2025-01-10 11:13:45 +0100

    modified:   02Anagrama/Anagrama.c

--- 02Anagrama/Anagrama.c
+++ 02Anagrama/Anagrama.c
@@ -6,7 +6,7 @@
 /*   By: alexgarc4 <alexgarc4@student.42barcelon    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 10:00:44 by alexgarc4         #+#    #+#             */
-/*   Updated: 2025/01/10 01:55:02 by alexgarc4        ###   ########.fr       */
+/*   Updated: 2025/01/10 11:11:39 by alexgarc4        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,11 +49,11 @@ int	main(int argc, char *argv[])
 	}
 	if (anagrama(argv[1], argv[2]))
 	{
-		printf("%s and %s are anagrams\n", argv[1], argv[2]);
+		printf("%s y %s son anagramas\n", argv[1], argv[2]);
 	}
 	else
 	{
-		printf("%s and %s are not anagrams\n", argv[1], argv[2]);
+		printf("%s y %s no son anagramas\n", argv[1], argv[2]);
 	}
 	return (0);
 }
